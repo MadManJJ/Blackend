4d1edbdb2958b871490966181446741e7eecd142
Chainakrit Manit <6733049121@student.chula.ac.th>
2025-02-28 20:33:03 +0700

    fix

--- controllers/reservations.js
+++ controllers/reservations.js
@@ -5,6 +5,36 @@ const Shop = require("../models/Shop");
 // @route GET /api/v1/reservations/:id
 // @access -
 exports.getReservation = async (req, res, next) => {
+  try {
+    const reservation = await Reservation.findById(req.params.id).populate({
+      path: "shop",
+      select: "-_id",
+    });
+    if (!reservation) {
+      return res.status(404).json({
+        success: false,
+        message: `No reservation with the id of ${req.params.id}`,
+      });
+    }
+
+    if (req.user.role !== "admin" && reservation.user != req.user) {
+      return res.status(403).json({
+        success: false,
+        message: `You are not authorize to access this reservation`,
+      });
+    }
+
+    res.status(200).json({ success: true, data: reservation });
+  } catch (err) {
+    console.log(err.stack);
+    res.status(500).json({ success: false, msg: "Cannot get reservation" });
+  }
+};
+
+// @desc Get all reservations
+// @route GET /api/v1/reservations/
+// @access -
+exports.getReservations = async (req, res, next) => {
   let query;
 
   if (req.user.role !== "admin") {
@@ -52,13 +82,6 @@ exports.getReservation = async (req, res, next) => {
       .status(500)
       .json({ success: false, message: "Cannot find Reservation" });
   }
-};
-
-// @desc Get all reservations
-// @route GET /api/v1/reservations/
-// @access -
-exports.getReservations = async (req, res, next) => {
-  res.status(200).json({ msg: "All reservations retrieved successfully" });
 };
 
 // @desc Create reservation
