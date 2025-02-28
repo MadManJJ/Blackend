adf987cb316f6a47b82ba92550d460b974d51eef
siwakorn <jeensiwakorn001@gmail.com>
2025-02-28 21:58:08 +0700

    Added sendTokenResponse function

--- controllers/auth.js
+++ controllers/auth.js
@@ -65,3 +65,23 @@ exports.logout = async (req, res, next) => {
     data: {},
   });
 };
+
+const sendTokenResponse = (user, statusCode, res) => {
+  const token = user.getSignedJwtToken();
+
+  const option = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000 // use milisec JWT_COOKIE_EXPIRE is a day
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === "production") {
+    options.secure = true;
+  }
+
+  res
+    .status(statusCode)
+    .cookie("token", token, options)
+    .json({ success: true, token });
+};
