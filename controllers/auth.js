88ca4db44fc473542a5854c3eaec40e6ea8a87a1
siwakorn <jeensiwakorn001@gmail.com>
2025-02-28 20:29:58 +0700

    Added logic in register contoller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,25 @@
 const User = require("../models/User");
 
+// POST public
 exports.register = async (req, res, next) => {
-  res.status(200).json({ msg: "User registered successfully" });
+  try {
+    const { name, email, password, role, telephone } = req.body;
+
+    const user = await User.create({
+      name,
+      email,
+      role,
+      password,
+      telephone,
+    });
+
+    // const token = user.getSignedJwtToken();
+    // res.status(200).json({ success: true, token });
+    sendTokenResponse(user, 200, res);
+  } catch (err) {
+    res.status(400).json({ success: false });
+    console.log(err.stack);
+  }
 };
 
 exports.login = async (req, res, next) => {
