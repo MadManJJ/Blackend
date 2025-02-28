73d7f03597871dd594a43a681ded395fe6613a1a
siwakorn <jeensiwakorn001@gmail.com>
2025-02-28 20:52:15 +0700

    Implemented login controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -21,9 +21,29 @@ exports.register = async (req, res, next) => {
     console.log(err.stack);
   }
 };
-
 exports.login = async (req, res, next) => {
-  res.status(200).json({ msg: "Login Successful" });
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "Please provide an email and password" });
+  }
+
+  const user = await User.findOne({ email }).select("+password"); // shorthand for User.findOne({ email : email }).select("+password");
+
+  if (!user) {
+    return res.status(400).json({ success: false, msg: "Invalid credentials" });
+  }
+
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return res.status(401).json({ success: false, msg: "Invalid credentials" });
+  }
+
+  // const token = user.getSignedJwtToken();
+  // res.status(200).json({ success: true, token, msg: "Login Successful" });
+  sendTokenResponse(user, 200, res);
 };
 
 exports.getMe = async (req, res, next) => {
