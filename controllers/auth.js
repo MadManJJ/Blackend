fa8c788cf3eb43412574623103d55e2b4213e881
siwakorn <jeensiwakorn001@gmail.com>
2025-02-28 21:59:12 +0700

    Fixed variable name (option -> options) in sendTokenResponse

--- controllers/auth.js
+++ controllers/auth.js
@@ -69,7 +69,7 @@ exports.logout = async (req, res, next) => {
 const sendTokenResponse = (user, statusCode, res) => {
   const token = user.getSignedJwtToken();
 
-  const option = {
+  const options = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000 // use milisec JWT_COOKIE_EXPIRE is a day
     ),
