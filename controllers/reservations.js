3de79848703e12d82dfe86e4f111a8ad2e571c1a
Chainakrit Manit <6733049121@student.chula.ac.th>
2025-02-28 20:23:02 +0700

    finish getReservation

--- controllers/reservations.js
+++ controllers/reservations.js
@@ -5,7 +5,30 @@ const Shop = require("../models/Shop");
 // @route GET /api/v1/reservations/:id
 // @access -
 exports.getReservation = async (req, res, next) => {
-  res.status(200).json({ msg: "Reservation retrieved successfully" });
+  try {
+    const reservation = await Reservation.findById(req.params.id).populate({
+      path: "shop",
+      select: "-_id",
+    });
+    if (!reservation) {
+      return res.status(404).json({
+        success: false,
+        message: `No reservation with the id of ${req.params.id}`,
+      });
+    }
+
+    if (req.user.role !== "admin" && reservation.user != req.user) {
+      return res.status(403).json({
+        success: false,
+        message: `You are not authorize to access this reservation`,
+      });
+    }
+
+    res.status(200).json({ success: true, data: reservation });
+  } catch (err) {
+    console.log(err.stack);
+    res.status(500).json({ success: false, msg: "Cannot get reservation" });
+  }
 };
 
 // @desc Get all reservations
